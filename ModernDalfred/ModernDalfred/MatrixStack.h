c2ea5cdb402bbd1874dc6c42a198643e2079010a
Brian Murray <brian@murraybrian.com>
2013-02-24 17:06:53 -0600

    Deleted a few legacy lines from main.cpp

---
+++ ModernDalfred/ModernDalfred/MatrixStack.h
@@ -0,0 +1,30 @@
+#ifndef MATRIX_STACK_H
+#define MATRIX_STACK_H
+
+#include <stack>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp> 
+
+using namespace std;
+
+class MatrixStack {
+public:
+	// initialized to identity
+	glm::mat4 active;
+
+	// Active matrix is left as the identity matrix
+	MatrixStack();
+
+	// push the current active matrix onto the top of the stack
+	void push();
+
+	// pop the top matrix off the stack and set it as the active matrix
+	void pop();
+
+private:
+	stack<glm::mat4> matStack;
+};
+
+
+#endif
--- ModernDalfred/ModernDalfred/main.cpp
+++ ModernDalfred/ModernDalfred/main.cpp
@@ -3,6 +3,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp> 
+#include "MatrixStack.h"
 
 using namespace std;
 
@@ -12,31 +13,17 @@ struct WindowData
 	GLint handle;
 } window;
 
+MatrixStack mvs;
+
 void DisplayFunc()
 {
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_PROJECTION);
-	glm::mat4 projection_matrix = glm::perspective(30.0f, float(window.width) / float(window.height), 1.0f, 10.0f);
-	glLoadMatrixf(glm::value_ptr(projection_matrix));
-//	glLoadIdentity();
-//	gluPerspective(30, double(window.width) / double(window.height), 1, 10);
-	glViewport(0, 0, window.width, window.height);
-	glMatrixMode(GL_MODELVIEW);
-	glm::mat4 modelview_matrix;
-	modelview_matrix = glm::translate(modelview_matrix, glm::vec3(0.0f, 0.0f, -5.0f));
-	glLoadMatrixf(glm::value_ptr(modelview_matrix));
 
-	//glLoadIdentity();
-	//glTranslated(0, 0, -5);
-	glBegin(GL_TRIANGLES);
-	glColor3d(1, 0, 0);
-	glVertex2d(0, 1);
-	glColor3d(0, 1, 0);
-	glVertex2d(1, 0);
-	glColor3d(0, 0, 1);
-	glVertex2d(-1, 0);
-	glEnd();
+
+	glViewport(0, 0, window.width, window.height);
+
+
 	glutSwapBuffers();
 	glutPostRedisplay();
 }
