e8d7edfa208a77f2603655cb20c5516b7dea0480
Brian Murray <brian@murraybrian.com>
2013-03-13 00:17:10 -0500

    Fixed weird texture shimmering

--- ModernDalfred/ModernDalfred/TextureManager.cpp
+++ ModernDalfred/ModernDalfred/TextureManager.cpp
@@ -31,11 +31,12 @@ GLuint TextureManager::loadTexture(const char *filename, const char* texName) {
 		image.getSize().y, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void*) image.getPixelsPtr());
 
 	// set up texture scaling parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	// set up texture repeat parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 	textures[texName] = tid;
 
